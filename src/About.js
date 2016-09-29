582df42af012ba2a190041e5554d577249e1ea5f
meeniebeanie <minyen.j@gmail.com>
2016-09-29 15:06:13 +0800

    added about me copy

--- src/About.js
+++ src/About.js
@@ -7,6 +7,7 @@ class About extends Component {
       <div className="About-panel" id="about">
         <h1 className="title">About me</h1>
         <hr/>
+        <p>I believe that technology makes the world a better place. Constantly excited by creative ideas, I love collaborating with passionate people to deliver results. Let’s have a chat about changing the world, together.</p>
       </div>
     )
   }
