018a291d94c3683fb0f8c14fe34f16b6de03ad00
meeniebeanie <minyen.j@gmail.com>
2016-09-30 23:16:34 +0800

    changed portfolio component to origin and added sticky underline

--- src/Showcase.js
+++ src/Showcase.js
@@ -4,48 +4,74 @@ import Cheq from './Showcase/Cheq.js';
 import Karakom from './Showcase/Karakom.js';
 import Milieu from './Showcase/Milieu.js';
 import Tompang from './Showcase/Tompang.js';
-import FeatureButton from './Components/FeatureButton.js';
 
 class Showcase extends Component {
   constructor(){
     super();
     this.state = {
-      _showContent: "karakom"
+      p1: false,
+      p2: false,
+      p3: false,
+      p4: false
     }
   }
 
-  _triggerChange(title) {
-    this.setState({
-      _showContent: title
-    })
+  _setExpState(exp){
+    const defaultState = {
+      p1: false,
+      p2: false,
+      p3: false,
+      p4: false
+    }
+    const whenClicked = {
+      [exp]: true
+    }
+    const newState = Object.assign({}, defaultState, whenClicked)
+    this.setState(newState)
+  }
+
+  _getClassName(exp){
+    if (this.state[exp]) return 'borderbottom'
+    return ''
+  }
+
+  _P1click(){
+    this._setExpState('p1')
+  }
+  _P2click(){
+    this._setExpState('p2')
+  }
+  _P3click(){
+    this._setExpState('p3')
+  }
+  _P4click(){
+    this._setExpState('p4')
   }
 
   render(){
-    switch(this.state._showContent) {
-      case "Cheq":
-        this.state.content = <Cheq />
-        break;
-      case "Milieu":
-        this.state.content = <Milieu />
-        break;
-      case "Tompang":
-        this.state.content = <Tompang />
-        break;
-      default:
-        this.state.content = <Karakom />
-        break;
+
+    let Content = <Karakom />;
+
+    if (this.state.p2){
+      Content = <Tompang />
+    }
+    else if (this.state.p3){
+      Content = <Milieu />
+    }
+    else if (this.state.p4){
+      Content = <Cheq />
     }
 
     return (
       <div className="Showcase-panel">
         <h1 className="title">Portfolio</h1>
         <div className="projNav">
-          <FeatureButton title="Karakom" cb={this._triggerChange.bind(this)} />
-          <FeatureButton title="Cheq" cb={this._triggerChange.bind(this)} />
-          <FeatureButton title="Milieu" cb={this._triggerChange.bind(this)} />
-          <FeatureButton title="Tompang" cb={this._triggerChange.bind(this)} />
+          <div className={this._getClassName('p1')} onClick={this._P1click.bind(this)}>Karakom</div>
+          <div className={this._getClassName('p2')} onClick={this._P2click.bind(this)}>Tompang</div>
+          <div className={this._getClassName('p3')} onClick={this._P3click.bind(this)}>Milieu</div>
+          <div className={this._getClassName('p4')} onClick={this._P4click.bind(this)}>Cheq</div>
         </div>
-        {this.state.content}
+        {Content}
       </div>
     )
   }
