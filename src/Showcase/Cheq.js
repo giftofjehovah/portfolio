02f9eb327db5c4ec68c4792b8cde2640a96519df
meeniebeanie <minyen.j@gmail.com>
2016-10-01 10:01:01 +0800

    updated all content for portfolio component

--- src/Showcase/Cheq.js
+++ src/Showcase/Cheq.js
@@ -8,10 +8,38 @@ class Cheq extends Component {
         <h3>CHEQ</h3>
         <div className="parentflex">
           <div>
-            <p>Cheq is great</p>
+            <div>
+              <p><a href="https://meeniebeanie.github.io/karakom-/">Cheq</a> is great</p>
+              <p>Short description about Milieu.</p>
+            </div>
+            <div className="parentflex">
+              <div>
+                <h5>What I like about it:</h5>
+                <ul>
+                <li>this</li>
+                <li>this</li>
+                <li>this</li>
+                </ul>
+              </div>
+              <div>
+                <h5>What I would change:</h5>
+                <ul>
+                <li>this</li>
+                <li>this</li>
+                <li>this</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+            <h5>Built Using</h5>
+            <ul>
+            <li>HTML & CSS</li>
+            <li>Javascript</li>
+            </ul>
+            </div>
           </div>
           <div>
-            <p>Cheq is quite great</p>
+            <iframe src="https://media.giphy.com/media/adwyFv25LoTHG/giphy.gif" width="480" height="342" frameBorder="0" allowFullScreen></iframe>
           </div>
         </div>
       </div>
--- src/Showcase/Tompang.js
+++ src/Showcase/Tompang.js
@@ -8,10 +8,38 @@ class Tompang extends Component {
         <h3>TOMPANG</h3>
         <div className="parentflex">
           <div>
-            <p><a href="http://powerful-shelf-49329.herokuapp.com/">Tompang</a> is great</p>
+            <div>
+              <p><a href="https://meeniebeanie.github.io/karakom-/">Tompang</a> is great</p>
+              <p>Short description about Milieu.</p>
+            </div>
+            <div className="parentflex">
+              <div>
+                <h5>What I like about it:</h5>
+                <ul>
+                <li>this</li>
+                <li>this</li>
+                <li>this</li>
+                </ul>
+              </div>
+              <div>
+                <h5>What I would change:</h5>
+                <ul>
+                <li>this</li>
+                <li>this</li>
+                <li>this</li>
+                </ul>
+              </div>
+            </div>
+            <div>
+            <h5>Built Using</h5>
+            <ul>
+            <li>HTML & CSS</li>
+            <li>Javascript</li>
+            </ul>
+            </div>
           </div>
           <div>
-            <p>Tompang is quite great</p>
+            <iframe src="https://media.giphy.com/media/adwyFv25LoTHG/giphy.gif" width="480" height="342" frameBorder="0" allowFullScreen></iframe>
           </div>
         </div>
       </div>
