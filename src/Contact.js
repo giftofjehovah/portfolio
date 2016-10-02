4371a0069ee8d7c5e60f625670cff17b15638b95
meeniebeanie <minyen.j@gmail.com>
2016-10-02 13:47:26 +0800

    styled contact me component

--- src/Contact.js
+++ src/Contact.js
@@ -2,11 +2,61 @@ import React, { Component } from 'react';
 import './style/App.css';
 
 class Contact extends Component {
+  constructor(){
+    super();
+    this.state = {
+      showContact: false,
+    }
+  }
+
+  _contactClick(){
+    this.setState({
+      showContact: !this.state.showContact
+    });
+  }
+
   render(){
+
+    let contactLinks;
+    let triangle;
+
+    if (this.state.showContact) {
+      triangle =
+      <div className="triangle">
+      </div>;
+
+      contactLinks =
+        <div className="Contact-info">
+        <div>
+          <a href="#about">About</a>
+        </div>
+
+        <hr className="contact-hr"/>
+
+        <div>
+          <a href="#education">Education</a><br/>
+        </div>
+
+        <hr className="contact-hr"/>
+
+        <div>
+          <a href="#work">Work</a><br/>
+        </div>
+
+        <hr className="contact-hr"/>
+
+        <div>
+          <a href="#contact">Let's Talk</a><br/>
+        </div>
+        </div>;
+    }
+
     return(
-      <div className="Contact-panel">
-        <h1 className="title">Contact Me</h1>
-        <hr/>
+      <div>
+        <div onClick={this._contactClick.bind(this)} className="Contact-bubble">
+        </div>
+        {contactLinks}
+        {triangle}
       </div>
     )
   }
